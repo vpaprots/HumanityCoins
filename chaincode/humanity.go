02afa407dc293f18ec4ce829102de080f5b980af
szlaci83 <szlaci83@gmail.com>
2016-08-11 06:26:26 +0100

    multiple user initiation

--- chaincode/humanity.go
+++ chaincode/humanity.go
@@ -1,5 +1,5 @@
 /*  This chaincode represents a humanity point system.....
-  TODO:add more description
+    TODO:add more description
 
  */
 package main
@@ -19,94 +19,83 @@ const (
 	large = 5 * iota //10
 )
 
-//users can recieve 3 types of thanks: ta, thanks, bigthanks
-//and a message stating their good deed
-type thank struct{
-	Thanker string `json:"name"`    //person who gives the "thank"
-	ThankType string `json:"type"`  //number of points given
-	Message string `json:"message"` //the reason for giving thanks
-}
-
-//each user has an ID a balance and an array of thanks
-//users can gain points by receiving thanks valued at different level (1,5,10 poinst)
+//Each user has an ID a balance and an array of thanks
+//users can gain points by receiving thanks valued at different level (small, medium, large)
 type entity struct {
-	UserID string `json:"name"`
-	Balance int `json:"balance"`   //points received by thanks
-	Thanks []thank `json:"thanks"` //list of the thanks received
+	UserID    string `json:"name"`
+	Balance   int `json:"balance"`   //points received by thanks
+	ThankList []thank `json:"thank"` //list of the thanks received
 }
 
+//Each thank contains:
+//- the name of the "giver"
+//- one of the three types of thanks: ta, thanks, bigthanks
+//- and a message stating their good deed
+type thank struct{
+	Thanker    string `json:"name"`    //person who gives the "thank"
+	ThankType  string `json:"type"`  //number of points given
+	Message    string `json:"message"` //the reason for giving thanks
+}
 
-//helper method to add a thank to the slice of thanks inside entity struct
+//AddThank method adds a thank to the slice of thanks inside entity struct.
 func (e *entity) AddThank(t thank) []thank {
-	e.Thanks = append(e.Thanks, t)
-	return e.Thanks
+	e.ThankList = append(e.ThankList, t)
+	return e.ThankList
 }
 
-//receiver for functions
+//HumanityChaincode is the receiver for chaincode functions
 type HumanityChaincode struct{
 }
 
-
-//init function
-//Function to initialize chaincode (add a single entity and points to start with)
-//TODO: make it work for more entities
+//Init function to initialize chaincode add entities and points to start with to the ledger.
+//TODO: check the work for more entities
 func (t *HumanityChaincode) Init(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	var userID string // name of the user to be registered on the chain
 	var pointsToAdd int //points to start with (default=0)
 	var err error
 
 	//get attributes from args
-	if len(args) < 1 {
-		return nil, errors.New("Incorrect number of args. Minimum one expected.(entity name)")
+	if len(args) %2 != 0  {
+		return nil, errors.New("Incorrect number of args. Needs to be even: (ID, points)")
 	}
-	if len(args) > 2 {
-		return nil, errors.New("Incorrect number of args. Maximum 2 expected:(entity name, points)")
-	}
-	if len(args) == 1 {
-		pointsToAdd = 0
-	}else{
-		pointsToAdd, err = strconv.Atoi(args[1])
+
+	//fill the db from args
+	for index := 0; index < len(args); index += 2{
+		userID = args[index]
+		pointsToAdd, err = strconv.Atoi(args[index + 1])
 		if err != nil {
-			return nil, errors.New("Expecting integer value for initial points")
+	    		return nil, errors.New("Expecting integer value for initial points")
+	    	}
+		entityObj := entity{}
+		entityObj.ThankList = []thank{}
+		entityObj.Balance = pointsToAdd
+		entityObj.UserID = userID
+
+		//convert struct to JSON
+		entityJson, err := json.Marshal(entityObj)
+		if err != nil || entityJson == nil {
+			return nil, errors.New("Converting entity struct to JSON failed")
+		}
+
+		//write attributes into ledger
+		err = stub.PutState(userID, entityJson)
+		if err != nil {
+			fmt.Printf("Error: could not update ledger")
+			return nil, err
 		}
 	}
 
-	//fill the struct from args
-	userID = args[0]
-	entityObj := entity{}
-	entityObj.Thanks = []thank{}
-	entityObj.Balance = pointsToAdd
-	entityObj.UserID = userID
-
-	//convert struct to JSON
-	entityJson, err := json.Marshal(entityObj)
-	if err != nil || entityJson == nil {
-		return nil, errors.New("Converting entity struct to JSON failed")
-	}
-
-
-	//write attributes into ledger
-	err = stub.PutState(userID, entityJson)
-	if err != nil {
-		fmt.Printf("Error: could not update ledger")
-		return nil, err
-	}
-
-	fmt.Printf("Humanity points chaincode ready.\n")
+	fmt.Printf("Humanity points chaincode initialization ready.\n")
 	return nil, nil
 }
 
-
-//get a random user who is at a certain level...
+//getRandomUser gets a random user who is at a certain level.
 func (t *HumanityChaincode) getRandomUser(stub *shim.ChaincodeStub,function string,args []string) ([]byte, error) {
 
 	return nil,nil
 }
 
-//function to receive a thank
-//we expect:
-//arg 0: ID (string)
-//arg 1: thank (json)
+//addThanks function to receive a thank
 func (t *HumanityChaincode) addThanks(stub *shim.ChaincodeStub,function string, args []string) ([]byte, error) {
 	var userID string
 	//var oldBalance int
@@ -150,7 +139,7 @@ func (t *HumanityChaincode) addThanks(stub *shim.ChaincodeStub,function string, 
 		return nil, errors.New("Error: No account exists for user.")
 		//TODO
 	}
-	
+
 	//convert JSON to struct
 	entityObj := entity{}
 	err = json.Unmarshal(entityJSON, &entityObj)
@@ -160,7 +149,7 @@ func (t *HumanityChaincode) addThanks(stub *shim.ChaincodeStub,function string, 
 
 	//add points:
 	entityObj.Balance = entityObj.Balance + pointsToAdd
-	
+
 
 	//add the thankObject to the thank array of the entityObject:
 	entityObj.AddThank(thankObj)
@@ -183,6 +172,7 @@ func (t *HumanityChaincode) addThanks(stub *shim.ChaincodeStub,function string, 
 
 }
 
+//Invoke function to invoke different functions in chaincode
 func (t *HumanityChaincode) Invoke(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if function == "addThanks" {
 		//Add points to a member
@@ -195,7 +185,7 @@ func (t *HumanityChaincode) Invoke(stub *shim.ChaincodeStub, function string, ar
 	return nil, errors.New("Received unknown function invocation")
 }
 
-//query returns the whole JSON for the userID
+//Query queries the ledger for a given ID and returns the whole JSON for the userID
 func (t *HumanityChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
 	if len(args) != 1 {
 		return nil, errors.New("Invalid number of arguments, expected 1")
@@ -207,12 +197,11 @@ func (t *HumanityChaincode) Query(stub *shim.ChaincodeStub, function string, arg
 		return nil, errors.New("Cannot get user data from chain.")
 	}
 
-
 	fmt.Printf("Query Response: %s\n", dataJson)
 	return dataJson, nil
 }
 
-//Main function
+//main function to start chaincode
 func main() {
 	err := shim.Start(new(HumanityChaincode))
 	if err != nil {
