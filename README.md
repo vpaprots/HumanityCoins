9ac6b6888d9ea13e4f1ca13a8bae03d42c9df6c2
szlaci83 <szlaci83@gmail.com>
2016-11-08 04:22:17 +0000

    finalised readme

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # HumanityCoins
+
+<!--- [![Deploy to Bluemix](https://bluemix.net/deploy/button.png)](https://bluemix.net/deploy?repository=https://github.com/vpaprots/HumanityCoins.git) --->
+
+
+# Application background
+
+This application will demonstrate a HumanityCoins point system where users can honour each other with HumanityCoins. Later HumanityCoins can also be collected by wearable devices, or smart meters, rewarding people saving resources (electricity, water, gas) or living a healthy lifestyle reported by their wearable device. To avoid abuse of the system these points are audited by a tweeting auditor (using twitter). The HumanityCoins points can be used by companies and government bodies to reward people doing good to their communities, health and to the environment.
+
+#Some technical details:
+
+Types of thanks:
+
+	1.small =1 HumanityCoins 
+	2.medium=5 HumanityCoins
+	3.large =10 HumanityCoins
+	
+Attributes of a user:
+
+	1. userID (unique string, will be used as key)
+	2. balance (int, computed points from the type of thank)
+	3. thanklist (string slice (array), array of the thanks recieved by the user)
+
+Attributes of a thank:
+
+	1.Thanker (the name of the person giving the thank)
+	2.ThankType (type of the thank small, medium, large) 
+	3.message (a small message explaining the thank, can be empty)
+
+#Project setup:
+In order to be able to try the project on your system, the hyperledger fabric needs to be set up:
+https://github.com/hyperledger/fabric/blob/master/docs/dev-setup/devenv.md
+
+The project should be placed under /root directory.
+Once you set up the system, you can start the test script to verify the settings. 
+/tests/humanity_test.sh
+
+You should get the following output:
+TESTS PASSED: 4/4
+
+Now you can start up your network of peers with HumanityCoins chaincode to serve the backend of the application.
+/scripts/4vpeers.sh
+
+The script uses the dockerised version of the chaincode from :
+szlaci83/humanitycoins_peer
+
+The backend of this application is running GoLang code on the 4 peer blockchain network on the mainframe, similar to IBM's High Security Bussiness Network. The front end application (a mobile app) can connect to this network via Rest API calls.
--- scripts/4vpeers.sh
+++ scripts/4vpeers.sh
@@ -3,14 +3,14 @@
 #                                                                                              #
 # Shell script to start up 4 validating peers using their .env files as environment variables  #
 # to simulate a HSBN (High Security Business Network).                                         #
-#                                                                                              #	
+#                                                                                              #
 # Laszlo Szoboszlai                                                                            #
 # 12/09/2016                                                                                   #
 ################################################################################################
 
 Banner () {
  echo -e "${CYAN}##################################################################################################"
- echo -e "#             This script will simulate a High Security Business Network by	                 #"
+ echo -e "#             This script will simulate a High Security Business Network by                      #"
  echo -e "#         starting up 4 Hyperledger fabric validating peers with security, privacy               #"
  echo -e "#                                    and PBFT consensus enabled.                                 #"
  echo -e "##################################################################################################${NC}"
@@ -18,7 +18,7 @@ Banner () {
 
 export GOROOT=/root/go
 export GOPATH=/root/git
-#export HLDGPATH=/root/git/src/github.com/hyperledger/fabric/build/bin
+export RESTPATH=$GOPATH/src/github.com/hyperledger/fabric/core/rest
 export HLDGPATH=$GOPATH/src/github.com/hyperledger/fabric/build/bin
 export CCROOT=/root/humanity
 
@@ -32,41 +32,41 @@ Banner
 echo -e "${RED}"
 read -p "This script will stop all running peers. Do you wish to continue?(y/n)" -n 1 -r
 echo -e "${NC}"
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]] 
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	clear
-	Banner
-	echo Clearing things up:
-	echo -Kill all previous processes 1/3
-	killall node > /dev/null 2>&1
-	killall peer > /dev/null 2>&1
-	killall membersrvc > /dev/null 2>&1
-	sleep 2
+        clear
+        Banner
+        echo Clearing things up:
+        echo -Kill all previous processes 1/3
+        killall node > /dev/null 2>&1
+        killall peer > /dev/null 2>&1
+        killall membersrvc > /dev/null 2>&1
+        sleep 2
 
-	echo -Docker clean up 2/3
-	docker stop $(docker ps -a -q)  > /dev/null 2>&1                #stop all containers
-	docker rm $(docker ps -a -q)  > /dev/null 2>&1   		#remove all containers
-	docker rmi $(docker images | grep dev-test) > /dev/null 2>&1    #remove all dev-test images
-	docker rmi $(docker images | grep dev-jdoe) > /dev/null 2>&1    #remove all dev-jdoe images
- 	sleep 2 
+        echo -Docker clean up 2/3
+        docker stop $(docker ps -a -q)  > /dev/null 2>&1                #stop all containers
+        docker rm $(docker ps -a -q)  > /dev/null 2>&1                  #remove all containers
+        docker rmi $(docker images | grep dev-test) > /dev/null 2>&1    #remove all dev-test images
+        docker rmi $(docker images | grep dev-jdoe) > /dev/null 2>&1    #remove all dev-jdoe images
+        sleep 2
 
-	echo -Delete all previous values in Hyperledger database 3/3
+        echo -Delete all previous values in Hyperledger database 3/3
         rm -rf /var/hyperledger/production
 
-	 TIMESTAMP=$(date +%d-%m-%Y-%H%M%S)
-   	 cd $CCROOT/logs
-   	 mkdir $TIMESTAMP
-   	 mv *.log $TIMESTAMP > /dev/null 2>&1
-	    	if [ $? -eq 0 ]; 
-		then
-       		 	echo -e  "${CYAN}Previous log files copied to $CCROOT/logs/$TIMESTAMP folder${NC}"
-		else
-      			echo There were no previous log files.
-  	 	fi
+         TIMESTAMP=$(date +%d-%m-%Y-%H%M%S)
+         cd $CCROOT/logs
+         mkdir $TIMESTAMP
+         mv *.log $TIMESTAMP > /dev/null 2>&1
+                if [ $? -eq 0 ];
+                then
+                        echo -e  "${CYAN}Previous log files copied to $CCROOT/logs/$TIMESTAMP folder${NC}"
+                else
+                        echo There were no previous log files.
+                fi
 
-else 
-    exit 1 
+else
+    exit 1
 fi
 
 #Copy most recent chaincode to Fabric handler
@@ -81,7 +81,7 @@ cd $HLDGPATH
 echo "Starting Membership and Security Server"
 ./membersrvc > $CCROOT/logs/MemberSrvc.log 2>&1 &
 sleep 5
-echo 
+echo
 
 #Start validating peers, each with its own .env file
 echo "Starting validating peers:"
@@ -97,6 +97,7 @@ docker run --rm --env-file $CCROOT/env/vp1.env szlaci83/humanitycoins_peer peer 
 echo "Waiting for initialization..."
 sleep 5
 
+
 #VP2:
 echo "Starting HyperLedger Fabric Validating Peer 3/4"
 docker run --rm --env-file $CCROOT/env/vp2.env szlaci83/humanitycoins_peer peer node start >  $CCROOT/logs/vp2.log 2>&1 &
@@ -109,22 +110,30 @@ docker run --rm --env-file $CCROOT/env/vp3.env szlaci83/humanitycoins_peer peer 
 echo "Waiting for initialization..."
 #sleep 5
 
-#echo login JIM to deploy
-#CORE_PEER_ADDRESS=0.0.0.0:30303 CORE_SECURITY_ENABLED=true CORE_SECURITY_PRIVACY=true peer network login jim -p 6avZQLwcUe9b
-#sleep 2
+echo login JIM to deploy
+CORE_PEER_ADDRESS=0.0.0.0:30303 CORE_SECURITY_ENABLED=true CORE_SECURITY_PRIVACY=true peer network login jim -p 6avZQLwcUe9b
+sleep 2
 
-#echo "deploying HumanityCoins chaincode:"
-#CHAIN_NAME=`CORE_PEER_ADDRESS=0.0.0.0:30303 CORE_SECURITY_ENABLED=true CORE_SECURITY_PRIVACY=true peer chaincode deploy -u jim -p github.com/hyperledger/fabric/examples/chaincode/go/humanity -c '{"Function":"Init", "Args": ["Laszlo","100","Juci","100"]}'`
+echo "deploying HumanityCoins chaincode: with test users laszlo: 100 points, Juci: 100 points"
+CHAIN_NAME=`CORE_PEER_ADDRESS=0.0.0.0:30303 CORE_SECURITY_ENABLED=true CORE_SECURITY_PRIVACY=true peer chaincode deploy -u jim -p github.com/hyperledger/fabric/examples/chaincode/go/humanity -c '{"Function":"Init", "Args": ["Laszlo","100","Juci","100"]}'`
 
 #if HumanityCoins deploy fails, have a go at this:
 #echo ex02
 #CHAIN_NAME=`CORE_PEER_ADDRESS=0.0.0.0:30303 CORE_SECURITY_ENABLED=true CORE_SECURITY_PRIVACY=true peer chaincode deploy -u jim -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'`
 
-
 #write chaincode hash to file
-#echo $CHAIN_NAME >> $CCROOT/logs/chain_name.log
+echo $CHAIN_NAME >> $CCROOT/logs/chain_name.log
 
-#echo -e "${CYAN}HumanityCoins chain:$CHAIN_NAME ${NC}"
+echo -e "${CYAN}HumanityCoins chain:$CHAIN_NAME ${NC}"
+
+echo "Start the Rest server:"
+cd $RESTPATH
+http-server -a 0.0.0.0 -p 5554 --cors  & #> $CCROOT/logs/rest-server.log 2>&1 &
 
 echo
 echo -e "${CYAN}High Security Bussiness Network Simulation ready.${NC}"
+
+
+
+
+
