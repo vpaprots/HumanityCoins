ae9ab6e7923038b3177caddd2da1f410aac4bfdb
Laszlo Szoboszlai <laszlo.szoboszlai@study.beds.ac.uk>
2017-08-22 23:34:04 +0100

    Update startFabric.sh

--- demo/startFabric.sh
+++ demo/startFabric.sh
@@ -18,12 +18,13 @@ cd basic-network
 ./start.sh
 
 # Now launch the CLI container in order to install, instantiate chaincode
-# and prime the ledger with our 10 cars
+# and prime the ledger with two users bob and alice with 20 and 10 initial points 
 docker-compose -f ./docker-compose.yml up -d cli
 
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode install -n humanity -v 1.0 -p github.com/humanity
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n humanity -v 1.0 -c '{"Args":["alice","10", "bob","20"]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
 sleep 10
+# invoke the addThank method to simulate a medium thank (5 points) is given from bob to alice in the system
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n humanity -c '{"Function":"addThanks", "Args": ["bob","{\"name\":\"alice\",\"type\":\"thankyou\",\"message\":\"for being good\"}"]}'
 
 printf "\nTotal execution time : $(($(date +%s) - starttime)) secs ...\n\n"
